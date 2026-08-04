5e3f9edefc7e19bbe94c51149af1491eb14983dc
Ananya K <ananyadevanga139@gmail.com>
2026-08-04 15:15:19 +0530

    Support local and production database connections

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,19 +3,26 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: Number(process.env.DB_PORT),
+        ssl: false,
+      }
+);
 
 pool.on("connect", () => {
   console.log("PostgreSQL Connected Successfully");
-});
-
-pool.on("error", (err) => {
-  console.log("Database Error:", err);
 });
 
 export default pool;
